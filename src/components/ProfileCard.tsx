4862e8c81b098be9c937a662681d4318600903e2
sk790 <saurabhk2890@gmail.com>
2024-06-30 09:50:42 +0530

    fetch the user and set the user information in frofile card

--- src/components/ProfileCard.tsx
+++ src/components/ProfileCard.tsx
@@ -1,66 +1,84 @@
-import Image from 'next/image'
-import Link from 'next/link'
-import React from 'react'
+import prisma from "@/lib/client";
+import { auth } from "@clerk/nextjs/server";
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
 
-export default function ProfileCard() {
+export default async function ProfileCard() {
+  const { userId } = auth();
+
+  if (!userId) return null;
+
+  const user = await prisma.user.findFirst({
+    where: {
+      Id: userId,
+    },
+    include: {
+      _count: {
+        select: {
+          followers: true,
+        },
+      },
+    },
+  });
+  if(!user) return null
+  console.log(user);
+
   return (
     <div className="p-4 bg-white rounded-lg shadow-md text-sm flex flex-col gap-6">
-    <div className="h-20 relative">
-      <Image
-        src={"/saurabh.jpg"}
-        alt=""
-        fill
-        className="rounded-md object-cover"
-      />
-      <Image
-        src={"/saurabh.jpg"}
-        alt=""
-        width={48}
-        height={48}
-        className="rounded-full object-cover w-12 h-12 absolute left-0 right-0 m-auto -bottom-6 ring-1 ring-white z-10"
-      />
-    </div>
-    <div className="h-20 flex flex-col gap-2 items-center">
-      <span className="font-semibold">
-        {/* {user.name && user.surname
-          ? user.name + " " + user.surname
-          : user.username} */}
-          username
-      </span>
-      <div className="flex items-center gap-4">
-        <div className="flex">
-          <Image
-            src="/demo.jpg"
-            alt=""
-            width={12}
-            height={12}
-            className="rounded-full object-cover w-3 h-3"
-          />
-          <Image
-            src="/demo.jpg"
-            alt=""
-            width={12}
-            height={12}
-            className="rounded-full object-cover w-3 h-3"
-          />
-          <Image
-            src="/demo.jpg"
-            alt=""
-            width={12}
-            height={12}
-            className="rounded-full object-cover w-3 h-3"
-          />
-        </div>
-        <span className="text-xs text-gray-500">
-          3k Followers
-        </span>
+      <div className="h-20 relative">
+        <Image
+          src={user && user.cover ? user.cover : "/demo.jpg"}
+          alt=""
+          fill
+          className="rounded-md object-cover"
+        />
+        <Image
+          src={user.avatar ? user.avatar : "/noAvatar.png"}
+          alt=""
+          width={48}
+          height={48}
+          className="rounded-full object-cover w-12 h-12 absolute left-0 right-0 m-auto -bottom-6 ring-1 ring-white z-10"
+        />
       </div>
-      <Link href={`/profile/saurabh`}>
-        <button className="bg-blue-500 text-white text-xs p-2 rounded-md">
-          My Profile
-        </button>
-      </Link>
+      <div className="h-20 flex flex-col gap-2 items-center">
+        <span className="font-semibold">
+          {user.name && user.surname
+          ? user.name + " " + user.surname
+          : user.username}
+        </span>
+        <div className="flex items-center gap-4">
+          <div className="flex">
+            <Image
+              src="/demo.jpg"
+              alt=""
+              width={12}
+              height={12}
+              className="rounded-full object-cover w-3 h-3"
+            />
+            <Image
+              src="/demo.jpg"
+              alt=""
+              width={12}
+              height={12}
+              className="rounded-full object-cover w-3 h-3"
+            />
+            <Image
+              src="/demo.jpg"
+              alt=""
+              width={12}
+              height={12}
+              className="rounded-full object-cover w-3 h-3"
+            />
+          </div>
+          <span className="text-xs text-gray-500">{user._count.followers} Followers</span>
+        </div>
+        <Link href={`/profile/saurabh`}>
+          <button className="bg-blue-500 text-white text-xs p-2 rounded-md">
+            My Profile
+          </button>
+        </Link>
+      </div>
     </div>
-  </div>
-  )
+  );
 }
