83f66d7ae9a6b600c18d33e5e37fc7e6f3c5b38a
sk790 <saurabhk2890@gmail.com>
2024-07-01 22:27:38 +0530

    change update user frofile component

--- src/components/rightMenu/UpdateButton.tsx
+++ src/components/rightMenu/UpdateButton.tsx
@@ -6,7 +6,7 @@ export default function UpdateButton() {
       className="bg-blue-500 p-2 mt-2 rounded-md text-white disabled:bg-opacity-50 disabled:cursor-not-allowed"
     //   disabled={pending}
     >
-      {/* {pending ? "Updating..." : "Update"} */}.
+      {/* {pending ? "Updating..." : "Update"} */}
       Update
     </button>
   );
--- src/components/rightMenu/UpdateUser.tsx
+++ src/components/rightMenu/UpdateUser.tsx
@@ -3,14 +3,15 @@
 import { User } from "@prisma/client";
 import React, { useState } from "react";
 import UpdateButton from "./UpdateButton";
+import Image from "next/image";
 
-export default function UpdateUser({ user }: { user?: User }) {
+export default function UpdateUser({ user }: { user: User }) {
   const [open, setOpen] = useState(false);
   const handleClose = () => {
     setOpen(false);
     // state.success && router.refresh();
   };
-  if(!user) return null
+  if (!user) return null;
 
   return (
     <div className="">
@@ -23,46 +24,23 @@ export default function UpdateUser({ user }: { user?: User }) {
       {open && (
         <div className="absolute w-screen h-screen top-0 left-0 bg-black bg-opacity-65 flex items-center justify-center z-50 ">
           <form
-            // action={(formData) =>
-            //   formAction({ formData, cover: cover?.secure_url || "" })
-            // }
             className="p-12 bg-white rounded-lg shadow-md flex flex-col gap-2 w-full md:w-1/2 xl:w-1/3 relative"
           >
-            {/* TITLE */}
             <h1>Update Profile</h1>
             <div className="mt-4 text-xs text-gray-500">
               Use the navbar profile to change the avatar or username.
             </div>
-            {/* COVER PIC UPLOAD */}
-            {/* <CldUploadWidget
-              uploadPreset="social"
-              onSuccess={(result) => setCover(result.info)}
-            >
-              {({ open }) => {
-                return (
-                  <div
-                    className="flex flex-col gap-4 my-4"
-                    onClick={() => open()}
-                  >
-                    <label htmlFor="">Cover Picture</label>
-                    <div className="flex items-center gap-2 cursor-pointer">
-                      <Image
-                        src={user.cover || "/noCover.png"}
-                        alt=""
-                        width={48}
-                        height={32}
-                        className="w-12 h-8 rounded-md object-cover"
-                      />
-                      <span className="text-xs underline text-gray-600">
-                        Change
-                      </span>
-                    </div>
-                  </div>
-                );
-              }}
-            </CldUploadWidget> */}
-
-            {/* WRAPPER */}
+            <label htmlFor="">Cover Picture</label>
+            <div className="flex items-center gap-2 cursor-pointer">
+              <Image
+                src={user.cover || "/noCover.png"}
+                alt=""
+                width={48}
+                height={32}
+                className="w-12 h-8 rounded-md object-cover"
+              />
+              <span className="text-xs underline text-gray-600">Change</span>
+            </div>
             <div className="flex flex-wrap justify-between gap-2 xl:gap-4">
               {/* INPUT */}
               <div className="flex flex-col gap-4">
@@ -71,7 +49,7 @@ export default function UpdateUser({ user }: { user?: User }) {
                 </label>
                 <input
                   type="text"
-                  placeholder={user.name || "Saurabh"}
+                  placeholder={user.name || "First Name"}
                   className="ring-1 ring-gray-300 p-[13px] rounded-md text-sm"
                   name="name"
                 />
@@ -82,36 +60,33 @@ export default function UpdateUser({ user }: { user?: User }) {
                 </label>
                 <input
                   type="text"
-                  placeholder={user.surname || "Nagar"}
+                  placeholder={user.surname || "Surname"}
                   className="ring-1 ring-gray-300 p-[13px] rounded-md text-sm"
                   name="surname"
                 />
               </div>
-              {/* INPUT */}
               <div className="flex flex-col gap-4">
                 <label htmlFor="" className="text-xs text-gray-500">
                   Description
                 </label>
                 <input
                   type="text"
-                  placeholder={user.description || "Life is beautiful..."}
+                  placeholder={user.description || "Description"}
                   className="ring-1 ring-gray-300 p-[13px] rounded-md text-sm"
                   name="description"
                 />
               </div>
-              {/* INPUT */}
               <div className="flex flex-col gap-4">
                 <label htmlFor="" className="text-xs text-gray-500">
                   City
                 </label>
                 <input
                   type="text"
-                  placeholder={user.city || "New York"}
+                  placeholder={user.city || "City"}
                   className="ring-1 ring-gray-300 p-[13px] rounded-md text-sm"
                   name="city"
                 />
               </div>
-              {/* INPUT */}
 
               <div className="flex flex-col gap-4">
                 <label htmlFor="" className="text-xs text-gray-500">
@@ -119,12 +94,11 @@ export default function UpdateUser({ user }: { user?: User }) {
                 </label>
                 <input
                   type="text"
-                  placeholder={user.school || "NIC"}
+                  placeholder={user.school || "School"}
                   className="ring-1 ring-gray-300 p-[13px] rounded-md text-sm"
                   name="school"
                 />
               </div>
-              {/* INPUT */}
 
               <div className="flex flex-col gap-4">
                 <label htmlFor="" className="text-xs text-gray-500">
@@ -132,32 +106,24 @@ export default function UpdateUser({ user }: { user?: User }) {
                 </label>
                 <input
                   type="text"
-                  placeholder={user.work || "Apple Inc."}
+                  placeholder={user.work || "Work"}
                   className="ring-1 ring-gray-300 p-[13px] rounded-md text-sm"
                   name="work"
                 />
               </div>
-              {/* INPUT */}
-
               <div className="flex flex-col gap-4">
                 <label htmlFor="" className="text-xs text-gray-500">
                   Website
                 </label>
                 <input
                   type="text"
-                  placeholder={user.website || "saurabh.dev"}
+                  placeholder={user.website || "Website"}
                   className="ring-1 ring-gray-300 p-[13px] rounded-md text-sm"
                   name="website"
                 />
               </div>
             </div>
             <UpdateButton />
-            {/* {state.success && (
-              <span className="text-green-500">Profile has been updated!</span>
-            )}
-            {state.error && (
-              <span className="text-red-500">Something went wrong!</span>
-            )} */}
             <div
               className="absolute text-xl right-2 top-3 cursor-pointer"
               onClick={handleClose}
