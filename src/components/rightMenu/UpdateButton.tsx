62cb64126a4d19b0a17704619ffdb959b18bb4ab
sk790 <saurabhk2890@gmail.com>
2024-07-02 07:11:58 +0530

    use useFormStatus hook

--- src/components/rightMenu/UpdateButton.tsx
+++ src/components/rightMenu/UpdateButton.tsx
@@ -1,13 +1,14 @@
 import React from "react";
+import { useFormStatus } from "react-dom";
 
 export default function UpdateButton() {
+  const {pending} = useFormStatus()
   return (
     <button
       className="bg-blue-500 p-2 mt-2 rounded-md text-white disabled:bg-opacity-50 disabled:cursor-not-allowed"
-    //   disabled={pending}
+      disabled={pending}
     >
-      {/* {pending ? "Updating..." : "Update"} */}
-      Update
+      {pending ? "Updating..." : "Update"}
     </button>
   );
 }
