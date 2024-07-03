b02b28cc321a4ba107cf88b305062d595ba9b09f
sk790 <saurabhk2890@gmail.com>
2024-07-03 17:55:12 +0530

    change path of friends on the navbar

--- src/components/MobileMenu.tsx
+++ src/components/MobileMenu.tsx
@@ -28,9 +28,9 @@ export default function MobileMenu() {
         />
       </div>
       {isOpen && (
-        <div className="absolute left-0 top-24 w-full h-[calc(100vh-96px)] bg-white flex flex-col items-center justify-center gap-8 font-medium text-xl z-10">
+        <div className="absolute left-0 top-24 w-full h-[calc(100vh-96px)] bg-white flex flex-col items-center justify-center gap-8 font-medium text-xl z-10" onClick={() => setIsOpen(false)}>
           <Link href="/">Home</Link>
-          <Link href="/">Friends</Link>
+          <Link href="/friends">Friends</Link>
           <Link href="/">Groups</Link>
           <Link href="/">Stories</Link>
           <Link href="/">Login</Link>
