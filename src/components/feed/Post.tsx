2081307a15fa30d347bcc28fd4d185c4c83ec6ce
sk790 <saurabhk2890@gmail.com>
2024-07-03 17:51:38 +0530

    created post component

--- src/components/feed/Post.tsx
+++ src/components/feed/Post.tsx
@@ -3,55 +3,63 @@ import PostInfo from "./PostInfo";
 import PostInteraction from "./PostInteraction";
 import { Suspense } from "react";
 import Comments from "./Comments";
+import { Post as PostType, User } from "@prisma/client";
+import { auth } from "@clerk/nextjs/server";
+import Link from "next/link";
 
-export default function Post() {
-    // const posts = []
+type feedPostType = PostType & { user: User } & {
+  likes: [{ userId: string }];
+} & { _count: { comments: number } };
+export default function Post({ post }: { post: feedPostType }) {
+  // const posts = []
+  const { userId } = auth();
   return (
     <div className="flex flex-col gap-4">
-    {/* USER */}
-    <div className="flex items-center justify-between">
-      <div className="flex items-center gap-4">
-        <Image
-          src={"/noAvatar.png"}
-          width={40}
-          height={40}
-          alt=""
-          className="w-10 h-10 rounded-full"
-        />
-        <span className="font-medium">
-          {/* {post.user.name && post.user.surname
-            ? post.user.name + " " + post.user.surname
-            : post.user.username} */}
-            username
-        </span>
-      </div>
-      <PostInfo/>
-    </div>
-    {/* DESC */}
-    <div className="flex flex-col gap-4">
-      {/* {post.img && ( */}
-        <div className="w-full min-h-96 relative">
-          <Image
-            src={"/demo.jpg"}
-            fill
-            className="object-cover rounded-md"
-            alt=""
-          />
+      {/* USER */}
+      <div className="flex items-center justify-between">
+        <div className="flex items-center gap-4">
+          <Link href={`/profile/${post.user.username}`}>
+            <Image
+              src={post.user.avatar || "/noAvatar.png"}
+              width={40}
+              height={40}
+              alt=""
+              className="w-10 h-10 rounded-full"
+            />
+          </Link>
+          <span className="font-medium">
+            {post.user.name && post.user.surname
+              ? post.user.name + " " + post.user.surname
+              : post.user.username}
+          </span>
         </div>
-      {/* )} */}
-      <p>this is post desciption</p>
+        {userId === post.user.Id && <PostInfo postId={post.Id} />}
+      </div>
+      {/* DESC */}
+      <div className="flex flex-col gap-4">
+        {post.img && (
+          <div className="w-full min-h-[500px] relative">
+            <Image
+              src={post.img}
+              fill
+              className="object-cover rounded-md"
+              alt=""
+            />
+          </div>
+        )}
+        <p>{post.desc}</p>
+      </div>
+      {/* INTERACTION */}
+      <Suspense fallback="Loading...">
+        <PostInteraction
+          postId={post.Id}
+          likes={post.likes.map((like) => like.userId)}
+          comments={post._count.comments}
+        />
+      </Suspense>
+      <Suspense fallback="Loading...">
+        <Comments postId={post.Id} />
+      </Suspense>
     </div>
-    {/* INTERACTION */}
-    <Suspense fallback="Loading...">
-      <PostInteraction
-        // postId={post.id}
-        // likes={post.likes.map((like) => like.userId)}
-        // commentNumber={post._count.comments}
-      />
-    </Suspense>
-    <Suspense fallback="Loading...">
-      <Comments />
-    </Suspense>
-  </div>
   );
 }
--- src/components/feed/PostInteraction.tsx
+++ src/components/feed/PostInteraction.tsx
@@ -1,16 +1,56 @@
-import Image from "next/image";
-import React from "react";
+"use client";
 
-export default function PostInteraction() {
-  const a = true;
+import { switchLike } from "@/lib/actions";
+import { useAuth } from "@clerk/nextjs";
+import Image from "next/image";
+import React, { useOptimistic, useState } from "react";
+
+export default function PostInteraction({
+  postId,
+  likes,
+  comments,
+}: {
+  postId: string;
+  likes: string[];
+  comments: number;
+}) {
+  const { isLoaded, userId } = useAuth();
+  const [likeState, setLikeState] = useState({
+    likeCount: likes.length,
+    isLiked: userId ? likes.includes(userId) : false,
+  });
+
+  const [optimisticLike, switchOptimisticLike] = useOptimistic(
+    likeState,
+    (state, value) => {
+      return {
+        likeCount: state.isLiked ? state.likeCount - 1 : state.likeCount + 1,
+        isLiked: !state.isLiked,
+      };
+    }
+  );
+
+  const likeAction = async () => {
+    switchOptimisticLike("");
+    try {
+      switchLike(postId);
+      setLikeState((state) => ({
+        likeCount: state.isLiked ? state.likeCount - 1 : state.likeCount + 1,
+        isLiked: !state.isLiked,
+      }));
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
   return (
     <div className="flex items-center justify-between text-sm my-4">
       <div className="flex gap-8">
         <div className="flex items-center gap-4 bg-slate-50 p-2 rounded-xl">
-          <form>
+          <form action={likeAction}>
             <button>
               <Image
-                src={a ? "/liked.png" : "/like.png"}
+                src={optimisticLike.isLiked ? "/liked.png" : "/like.png"}
                 width={16}
                 height={16}
                 alt=""
@@ -20,8 +60,7 @@ export default function PostInteraction() {
           </form>
           <span className="text-gray-300">|</span>
           <span className="text-gray-500">
-            {/* {optimisticLike.likeCount} */}
-            {21}
+            {optimisticLike.likeCount}
             <span className="hidden md:inline"> Likes</span>
           </span>
         </div>
@@ -35,8 +74,7 @@ export default function PostInteraction() {
           />
           <span className="text-gray-300">|</span>
           <span className="text-gray-500">
-            {/* {commentNumber} */}
-            {12}
+            {comments}
             <span className="hidden md:inline"> Comments</span>
           </span>
         </div>
