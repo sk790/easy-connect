4d7a519d52df5919cd86c5a0d51534c39ac66007
sk790 <saurabhk2890@gmail.com>
2024-07-13 15:30:08 +0530

    update prisma client file

--- src/lib/client.ts
+++ src/lib/client.ts
@@ -1,7 +1,5 @@
 import { PrismaClient } from "@prisma/client";
 
-const globalForPrisma = globalThis as unknown as {prisma:PrismaClient}
-
-const prisma = globalForPrisma.prisma || new PrismaClient()
+const prisma = new PrismaClient()
 
 export default prisma;
