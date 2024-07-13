4fb26327470c2cf58a84d2781b498f3b2f02cb09
sk790 <saurabhk2890@gmail.com>
2024-07-13 15:03:00 +0530

    update client.ts file

--- src/lib/client.ts
+++ src/lib/client.ts
@@ -5,5 +5,3 @@ const globalForPrisma = globalThis as unknown as {prisma:PrismaClient}
 const prisma = globalForPrisma.prisma || new PrismaClient()
 
 export default prisma;
-
-if(process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma
